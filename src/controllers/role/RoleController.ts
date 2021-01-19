2fc76692c4d43295524cbfadb177d12069b5e784
SanderBackx1 <r0752803@student.thomasmore.be>
2021-01-19 18:52:05 +0100

    Read

--- src/controllers/role/RoleController.ts
+++ src/controllers/role/RoleController.ts
@@ -3,6 +3,7 @@ import { CrudController } from "../CrudController";
 import { IUser, User, UserDocument } from "../../model/User";
 import RoleManager from "../../manager/RoleManager";
 import { Role } from "../../model/Role";
+import { Types } from "mongoose";
 
 const roleManager = RoleManager.Instance;
 
@@ -11,7 +12,16 @@ class RoleController extends CrudController {
     throw new Error("Not yet implemented");
   }
   public async read(req: Request, res: Response) {
-    throw new Error("Not yet implemented");
+    try {
+      const id = req.body;
+      if (!id) throw new Error("No id found");
+      if (!Types.ObjectId.isValid(id)) throw new Error("id is not valid");
+      const role = roleManager.getRoleById(id);
+      res.json(role);
+    } catch (err) {
+      throw err;
+    }
+    // throw new Error("Not yet implemented");
   }
   public async update(req: Request, res: Response) {
     throw new Error("Not yet implemented");
