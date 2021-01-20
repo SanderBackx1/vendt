6121d9cb071944b9c85ea0809a4c63db016624db
SanderBackx1 <r0752803@student.thomasmore.be>
2021-01-20 14:27:16 +0100

    add base endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { router as userRouter } from "./user/user";
 import { router as roleRouter } from "./role/role";
+import express, { Request, Response } from "express";
 
 import { Router } from "express";
 export const router = Router();
-
+router.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
+});
 router.use("/user", userRouter);
 router.use("/role", roleRouter);
