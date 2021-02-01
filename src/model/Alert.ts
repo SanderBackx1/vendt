58731bc96f444572753dc4c6b03fbdaec6d8a1c8
SanderBackx1 <r0752803@student.thomasmore.be>
2021-02-01 09:30:29 +0100

    alert schema

---
+++ src/model/Alert.ts
@@ -0,0 +1,24 @@
+import { model, Schema, Model, Document, Types } from "mongoose";
+
+export interface IAlert {
+  msg: string;
+  urgency: string;
+  user?: string;
+  machine?: string;
+  tag: string[];
+}
+
+export interface AlertDocument extends IAlert, Document {}
+
+const AlertSchema: Schema = new Schema(
+  {
+    msg: { type: String, required: true },
+    urgency: { type: String, required: true },
+    user: { type: Types.ObjectId, required: false, ref: "User" },
+    machine: { type: Types.ObjectId, required: false, ref: "Machine" },
+    tag: { type: [Array], required: true },
+  },
+  { timestamps: true }
+);
+
+export const Alert: Model<AlertDocument> = model("Alert", AlertSchema);
