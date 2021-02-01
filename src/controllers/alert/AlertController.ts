f9ec50bbfbea6f7fc2d9daf6d00820a736438521
SanderBackx1 <r0752803@student.thomasmore.be>
2021-02-01 10:18:27 +0100

    create read

--- src/controllers/alert/AlertController.ts
+++ src/controllers/alert/AlertController.ts
@@ -1,25 +1,56 @@
-import {Request, Response} from 'express'
+import { Request, Response } from "express";
 import { CrudController } from "../CrudController";
 import { Types } from "mongoose";
-import { Alert, IAlert} from "../../model/Alert";
+import { Alert, IAlert } from "../../model/Alert";
+import filter from "../../helpers/filter";
+class AlertController extends CrudController {
+  constructor() {
+    super();
+  }
+  public async create(req: Request, res: Response) {
+    const { qry } = req.body;
+    const { msg, urgency, tag, user, machine } = qry;
+    let alert: IAlert = {
+      msg,
+      tag,
+      urgency,
+    };
+    if(req.body?.machineId ||machine  ){
+        const machineId = req.body?.machineId ||machine 
+        if(!Types.ObjectId.isValid(machineId)) throw new Error("MachineId not valid")
+        alert = {...alert, machine:machineId}
+    }
+    if(user){
+        if(!Types.ObjectId.isValid(user)) throw new Error("user not valid")
+        alert = {...alert, user:req.body.user._id}
+    }
+    
+    const response = await Alert.create(alert);
+    res.json(response)
+  }
 
-class AlertController extends CrudController{
+  public async read(req: Request, res: Response) {
+    const {machine, id, user, tag} = req.query
 
-    constructor(){
-        super()
+    const query  = {
+      machine,
+      _id:id,
+      user,
+      tag,
     }
-    public async create(req:Request,res:Response){
-        throw new Error("not yet implemented")
-    }
-    public async read(req:Request,res:Response){
-        throw new Error("not yet implemented")
-    }
-    public async update(req:Request,res:Response){
-        throw new Error("not yet implemented")
-    }
-    public async delete(req:Request,res:Response){
-        throw new Error("not yet implemented")
-    }
-   
+
+    const toSearch = filter(query)
+
+    const response = await Alert.find(toSearch);
+    res.json(response)
+
+
+  }
+  public async update(req: Request, res: Response) {
+    throw new Error("update not yet implemented");
+  }
+  public async delete(req: Request, res: Response) {
+    throw new Error("delete not yet implemented");
+  }
 }
-export const alertController = new AlertController()
+export const alertController = new AlertController();
--- src/routes/alert/alert.ts
+++ src/routes/alert/alert.ts
@@ -1,45 +1,38 @@
 import { Router, Request, Response, response } from "express";
-import {alertController} from "../../controllers/alert/AlertController"
+import { alertController } from "../../controllers/alert/AlertController";
 export const router = Router();
 
-
-router.get(
-    "/",
-    async (req: Request, res: Response) => {
-      try {
-        await alertController.read(req,res)
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-    }
-);
+import { checkIfUpdate, secured, securedWithQuery } from "../../middleware";
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    await alertController.read(req, res);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 router.post(
-    "/",
-    async (req: Request, res: Response) => {
-      try {
-        await alertController.create(req,res)
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
+  "/",
+  securedWithQuery,
+  checkIfUpdate,
+  async (req: Request, res: Response) => {
+    try {
+      await alertController.create(req, res);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
     }
+  }
 );
-router.post(
-    "/",
-    async (req: Request, res: Response) => {
-      try {
-        await alertController.update(req,res)
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-    }
-);
-router.post(
-    "/delete",
-    async (req: Request, res: Response) => {
-      try {
-        await alertController.delete(req,res)
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-    }
-);
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await alertController.update(req, res);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.post("/delete", async (req: Request, res: Response) => {
+  try {
+    await alertController.delete(req, res);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
