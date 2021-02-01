6219b206cb44ba2c974c1d4ff3971b1ef72a429d
SanderBackx1 <r0752803@student.thomasmore.be>
2021-02-01 09:42:53 +0100

    add alert to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { router as companyRouter } from "./company/company";
 import { router as machineRouter } from "./machine/machine";
 import { router as inquiryRouter } from "./inquiry/inquiry";
 import { router as authRouter } from "./auth/auth";
+import { router as alertRouter } from "./alert/alert";
 import { Request, Response, Router } from "express";
 
 export const router = Router();
@@ -13,8 +14,9 @@ router.use("/company", companyRouter);
 router.use("/machine", machineRouter);
 router.use("/inquiry", inquiryRouter);
 router.use("/auth", authRouter);
-
+router.use("/alert", alertRouter);
 export const helloworldrouter = Router();
 helloworldrouter.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
+
