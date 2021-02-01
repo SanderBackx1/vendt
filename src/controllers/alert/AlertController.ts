67b34c990a43947b506f829ff291986e2c92bc71
SanderBackx1 <r0752803@student.thomasmore.be>
2021-02-01 09:40:53 +0100

    alertcontroller

---
+++ src/controllers/alert/AlertController.ts
@@ -0,0 +1,24 @@
+import {Request, Response} from 'express'
+import { CrudController } from "../CrudController";
+import { Types } from "mongoose";
+import { Alert, IAlert} from "../../model/Alert";
+
+class AlertController extends CrudController{
+
+    constructor(){
+        super()
+    }
+    public async create(req:Request,res:Response){
+        throw new Error("not yet implemented")
+    }
+    public async read(req:Request,res:Response){
+        throw new Error("not yet implemented")
+    }
+    public async update(req:Request,res:Response){
+        throw new Error("not yet implemented")
+    }
+    public async delete(req:Request,res:Response){
+        throw new Error("not yet implemented")
+    }
+   
+}
