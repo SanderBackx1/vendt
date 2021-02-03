f71d9054fab3f139031973d3f347677fa41050e9
SanderBackx1 <r0752803@student.thomasmore.be>
2021-02-03 14:17:52 +0100

    populate user in tag alerts

--- src/controllers/user/UserController.ts
+++ src/controllers/user/UserController.ts
@@ -195,7 +195,7 @@ class UserController extends CrudController {
     return await Alert.find({ user: userId }).populate('machine', 'name').sort({ createdAt: -1 });
   }
   private async fetchAlertsWithTags(tags: string[]) {
-    return await Alert.find({ tag: { $in: tags } }).populate('machine', 'name').sort({createdAt:-1});
+    return await Alert.find({ tag: { $in: tags } }).populate('machine', 'name').populate('user', 'firstname lastname').sort({createdAt:-1});
   }
 
   public async readAll(req: Request, res: Response) {
